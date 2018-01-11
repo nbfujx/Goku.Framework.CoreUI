4d4594e59c76c86a97a9af584f2e5a7e571f447d
nbfujx <nbfujx@qq.com>
2018-01-11 22:16:32 +0800

    新增模块展示页面

--- src/main/java/com/goku/coreui/sys/controller/sysmodule/impl/ModuleControllerImpl.java
+++ src/main/java/com/goku/coreui/sys/controller/sysmodule/impl/ModuleControllerImpl.java
@@ -1,9 +1,33 @@
 package com.goku.coreui.sys.controller.sysmodule.impl;
 
 import com.goku.coreui.sys.controller.sysmodule.ModuleController;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by nbfujx on 2018/1/10.
  */
+@Controller
+@RequestMapping("/sys/module")
 public class ModuleControllerImpl implements ModuleController {
+
+    @RequestMapping("/getListPage")
+    @RequiresPermissions(value={"sys:module:query"})
+    public String  list(Model model) {
+        return  "sys/module/list";
+    }
+
+    @RequestMapping("/addPage")
+    @RequiresPermissions(value={"sys:module:add"})
+    public String  add(Model model) {
+        return  "sys/module/edit";
+    }
+
+    @RequestMapping("/editPage")
+    @RequiresPermissions(value={"sys:module:edit"})
+    public String  edit(Model model) {
+        return  "sys/module/edit";
+    }
 }
--- src/main/java/com/goku/coreui/sys/controller/sysmodule/impl/ModuleRestControllerImpl.java
+++ src/main/java/com/goku/coreui/sys/controller/sysmodule/impl/ModuleRestControllerImpl.java
@@ -2,15 +2,20 @@ package com.goku.coreui.sys.controller.sysmodule.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.goku.coreui.sys.controller.sysmodule.ModuleRestController;
+import com.goku.coreui.sys.model.ext.Breadcrumb;
 import com.goku.coreui.sys.model.ext.TablePage;
 import com.goku.coreui.sys.service.SysModuleService;
 import com.goku.coreui.sys.service.SysUserService;
+import com.goku.coreui.sys.util.BreadcrumbUtil;
+import com.goku.coreui.sys.util.CamelUtil;
 import com.goku.coreui.sys.util.PageUtil;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by nbfujx on 2018/1/10.
@@ -23,10 +28,37 @@ public class ModuleRestControllerImpl implements ModuleRestController {
     SysModuleService sysModuleService;
 
     @Autowired
+    BreadcrumbUtil breadcrumbUtil;
+
+    @Autowired
     PageUtil pageUtil;
 
+    @Autowired
+    CamelUtil camelUtil;
+
+    @RequestMapping("/getListPage")
+    @RequiresPermissions(value={"sys:module:query"})
+    public String  list() {
+        List<Breadcrumb> Breadcrumbs= breadcrumbUtil.getBreadcrumbPath("sys/module/getListPage");
+        return JSON.toJSONString(Breadcrumbs);
+    }
+
+    @RequestMapping("/addPage")
+    @RequiresPermissions(value={"sys:module:add"})
+    public String  add() {
+        List<Breadcrumb> Breadcrumbs= breadcrumbUtil.getBreadcrumbPath("sys/module/addPage");
+        return JSON.toJSONString(Breadcrumbs);
+    }
+
+    @RequestMapping("/editPage")
+    @RequiresPermissions(value={"sys:module:edit"})
+    public String  edit() {
+        List<Breadcrumb> Breadcrumbs= breadcrumbUtil.getBreadcrumbPath("sys/module/editPage");
+        return JSON.toJSONString(Breadcrumbs);
+    }
+
     @RequestMapping("/getModuleForPaging")
-    //@RequiresPermissions(value={"sys:module:query"})
+    @RequiresPermissions(value={"sys:module:query"})
     public String  getModuleForPaging()
     {
         TablePage tp= pageUtil.getDataForPaging(sysModuleService.getModuleForPaging());
