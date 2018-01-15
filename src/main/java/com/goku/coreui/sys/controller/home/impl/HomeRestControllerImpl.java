3a11fa134bd0e3d11fe1db363482e6504f301925
nbfujx <nbfujx@qq.com>
2018-01-15 16:50:22 +0800

    调整了下按钮样式

--- src/main/java/com/goku/coreui/sys/controller/home/impl/HomeRestControllerImpl.java
+++ src/main/java/com/goku/coreui/sys/controller/home/impl/HomeRestControllerImpl.java
@@ -1,14 +1,21 @@
 package com.goku.coreui.sys.controller.home.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.goku.coreui.sys.controller.home.HomeRestController;
+import com.goku.coreui.sys.model.ext.Breadcrumb;
+import com.goku.coreui.sys.util.BreadcrumbUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by nbfujx on 2017/12/25.
@@ -16,5 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HomeRestControllerImpl implements HomeRestController {
+    @Autowired
+    BreadcrumbUtil breadcrumbUtil;
+
 
 }
